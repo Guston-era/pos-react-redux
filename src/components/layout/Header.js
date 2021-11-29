7c953eeae27dee3e65ab1f16196876958f2ca38c
GustoneraCodeBase <84548265+GustoneraCodeBase@users.noreply.github.com>
2021-11-29 21:58:29 +0300

    commenced header styling

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,7 +6,18 @@ const Header = () => {
       <div className="header-logo-section">
         <p>gPOS</p>
       </div>
-      <div className="header-main-area">Main Area</div>
+      <div className="header-main-area">
+        <div className="header-user-info">
+          <span>Gustone Alwanga</span>
+          <span>Tuesday, 2 Dec 2021</span>
+        </div>
+        <div className="header-client-info">
+          <span>REBS POS</span>
+        </div>
+        <div className="header-search-bar">
+          <input type="text" className="form-control" />
+        </div>
+      </div>
       <div className="header-right-area">Right Section</div>
     </div>
   );
--- src/components/layout/SideBar.js
+++ src/components/layout/SideBar.js
@@ -5,18 +5,24 @@ const SideBar = () => {
   return (
     <div className="main-side-bar">
       <div className="menu-item">
-        <div className="menu-item-active">
+        <div className="inner-menu-item menu-item-active">
           <i className="fa fa-dashboard fa-3x"></i>
         </div>
       </div>
       <div className="menu-item">
-        <i className="fa fa-cogs fa-3x"></i>
+        <div className="inner-menu-item">
+          <i className="fa fa-cogs fa-3x"></i>
+        </div>
       </div>
       <div className="menu-item">
-        <i className="fa fa-bar-chart fa-3x"></i>
+        <div className="inner-menu-item">
+          <i className="fa fa-bar-chart fa-3x"></i>
+        </div>
       </div>
       <div className="menu-item">
-        <i className="fa fa-sign-out fa-3x"></i>
+        <div className="inner-menu-item">
+          <i className="fa fa-sign-out fa-3x"></i>
+        </div>
       </div>
     </div>
   );
