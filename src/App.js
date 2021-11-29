7e2740e3c5d2ef207eafe2df43c05f8eefb060f2
GustoneraCodeBase <84548265+GustoneraCodeBase@users.noreply.github.com>
2021-11-29 20:34:58 +0300

    added fa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./css/App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <p> I wanna learn react</p>
+      <Button className="btn btn-warning">Go Away</Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import App from "./App";
 
 ReactDOM.render(
