9fcead58fcdb3db7ba6129c848d75e98c9cfa0e6
GustoneraCodeBase <84548265+GustoneraCodeBase@users.noreply.github.com>
2021-11-30 00:15:41 +0300

    added links to sidemenu

--- src/components/layout/SideBar.js
+++ src/components/layout/SideBar.js
@@ -1,24 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../../css/Layout.css";
 
 const SideBar = () => {
   return (
     <div className="main-side-bar">
-      <div className="menu-item">
+      <Link to="/dashboard" className="menu-item">
         <div className="inner-menu-item menu-item-active">
           <i className="fa fa-dashboard fa-3x"></i>
         </div>
-      </div>
-      <div className="menu-item">
+      </Link>
+      <Link to="/settings" className="menu-item">
         <div className="inner-menu-item">
           <i className="fa fa-cogs fa-3x"></i>
         </div>
-      </div>
-      <div className="menu-item">
+      </Link>
+      <Link to="/reports" className="menu-item">
         <div className="inner-menu-item">
           <i className="fa fa-bar-chart fa-3x"></i>
         </div>
-      </div>
+      </Link>
       <div className="menu-item">
         <div className="inner-menu-item">
           <i className="fa fa-sign-out fa-3x"></i>
