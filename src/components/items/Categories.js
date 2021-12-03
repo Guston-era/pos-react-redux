f8d2206efff08bf81d40e2015dfcf282a82f06e1
GustoneraCodeBase <84548265+GustoneraCodeBase@users.noreply.github.com>
2021-12-03 18:22:59 +0300

    added more content and items for sales content area

---
+++ src/components/items/Categories.js
@@ -0,0 +1,5 @@
+const Categories = () => {
+  return <div>Categories in slider like thing</div>;
+};
+
+export default Categories;
--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Categories from "../components/items/Categories";
 import Header from "../components/layout/Header";
 import SideBar from "../components/layout/SideBar";
 import "../css/App.css";
@@ -9,7 +10,13 @@ const Dashboard = () => {
       <Header />
       <div className="flexed-content">
         <SideBar active="dashboard" />
-        <div className="sales-content-area">Irun this era</div>
+        <div className="sales-content-area">
+          <hr />
+          <Categories />
+          <hr />
+          <p className="text-white">Pick Items</p>
+          <div className="row"></div>
+        </div>
         <div className="cart-content-area"></div>
       </div>
     </div>
