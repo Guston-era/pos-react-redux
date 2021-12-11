807c152e72eb58c1edbc39e5e6811451b77821e7
GustoneraCodeBase <84548265+GustoneraCodeBase@users.noreply.github.com>
2021-12-11 17:30:39 +0300

    added templates for data visualization

--- src/screens/subscreens/StatusReport.js
+++ src/screens/subscreens/StatusReport.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../css/subscreens.css";
 import MiniSideBarReports from "../../components/layout/MiniSideBarReports";
-import { Line } from "react-chartjs-2";
+import { Line, Bar, Pie, Doughnut } from "react-chartjs-2";
 import Chart from "chart.js/auto";
 import { ResizeObserver as ResizeObserverPolyfill } from "@juggle/resize-observer";
 
@@ -9,6 +9,93 @@ const StatusReport = () => {
   if (typeof window !== "undefined") {
     window.ResizeObserver = window.ResizeObserver || ResizeObserverPolyfill;
   }
+  // single bar chart
+  const SingleBarData = {
+    labels: ["January", "February", "March", "April", "May"],
+    datasets: [
+      {
+        id: 1,
+        label: "Rainfall",
+        // backgroundColor: "#ea7c69",
+        backgroundColor: [
+          "#B21F00",
+          "#C9DE00",
+          "#2FDE00",
+          "#00A6B4",
+          "#6800B4",
+        ],
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 2,
+        data: [65, 59, 80, 81, 56],
+      },
+    ],
+  };
+
+  // multi bar data
+  const MultiBarData = {
+    labels: ["January", "February", "March", "April", "May"],
+    datasets: [
+      {
+        id: 1,
+        label: "Rainfall",
+        backgroundColor: "#ea7c69",
+
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 2,
+        data: [65, 59, 80, 81, 56],
+      },
+      {
+        id: 2,
+        label: "Humidity",
+        backgroundColor: "green",
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 2,
+        data: [70, 49, 60, 51, 56],
+      },
+    ],
+  };
+  const pieData = {
+    labels: ["January", "February", "March", "April", "May"],
+    datasets: [
+      {
+        label: "Rainfall",
+        backgroundColor: [
+          "#B21F00",
+          "#C9DE00",
+          "#2FDE00",
+          "#00A6B4",
+          "#6800B4",
+        ],
+        hoverBackgroundColor: [
+          "#501800",
+          "#4B5000",
+          "#175000",
+          "#003350",
+          "#35014F",
+        ],
+        data: [65, 59, 80, 81, 56],
+      },
+      {
+        label: "Humidity",
+        backgroundColor: [
+          "#B21F00",
+          "#C9DE00",
+          "#2FDE00",
+          "#00A6B4",
+          "#6800B4",
+        ],
+        hoverBackgroundColor: [
+          "#501800",
+          "#4B5000",
+          "#175000",
+          "#003350",
+          "#35014F",
+        ],
+        data: [70, 49, 60, 51, 56],
+      },
+    ],
+  };
+
   return (
     <div className="flexed-content">
       <MiniSideBarReports active="status" />
@@ -16,22 +103,135 @@ const StatusReport = () => {
         <Line
           style={{ backgroundColor: "white" }}
           datasetIdKey="id"
-          data={{
-            labels: ["Jun", "Jul", "Aug"],
-            datasets: [
-              {
-                id: 1,
-                label: "m",
-                data: [5, 6, 7],
-              },
-              {
-                id: 2,
-                label: "d",
-                data: [3, 2, 1],
-              },
-            ],
+          data={MultiBarData}
+        />
+        <p>Line Graph</p>
+        {/* bar graph */}
+        <Bar
+          data={MultiBarData}
+          datasetIdKey="id"
+          options={{
+            title: {
+              display: true,
+              text: "Average Rainfall per month",
+              fontSize: 20,
+            },
+            legend: {
+              display: true,
+              position: "right",
+            },
           }}
         />
+        <p>MultiBarData</p>
+        <Bar
+          data={SingleBarData}
+          options={{
+            title: {
+              display: true,
+              text: "Average Rainfall per month",
+              fontSize: 20,
+            },
+            legend: {
+              display: true,
+              position: "right",
+            },
+          }}
+        />
+        <p>SingleBarData</p>
+        <Pie
+          data={MultiBarData}
+          options={{
+            title: {
+              display: true,
+              text: "Average Rainfall per month",
+              fontSize: 20,
+            },
+            legend: {
+              display: true,
+              position: "right",
+            },
+          }}
+        />
+        <p>Plain Multi Data Pie Graph</p>
+
+        <Pie
+          data={SingleBarData}
+          options={{
+            title: {
+              display: true,
+              text: "Average Rainfall per month",
+              fontSize: 20,
+            },
+            legend: {
+              display: true,
+              position: "right",
+            },
+          }}
+        />
+        <p>Single colorful Data Pie Graph</p>
+
+        <Pie
+          data={pieData}
+          options={{
+            title: {
+              display: true,
+              text: "Average Rainfall per month",
+              fontSize: 20,
+            },
+            legend: {
+              display: true,
+              position: "right",
+            },
+          }}
+        />
+        <p>Colorful Multi Data Pie Graph</p>
+        <Doughnut
+          data={MultiBarData}
+          options={{
+            title: {
+              display: true,
+              text: "Average Rainfall per month",
+              fontSize: 20,
+            },
+            legend: {
+              display: true,
+              position: "right",
+            },
+          }}
+        />
+        <p>Plain Multi Data Doughnut Graph</p>
+
+        <Doughnut
+          data={SingleBarData}
+          options={{
+            title: {
+              display: true,
+              text: "Average Rainfall per month",
+              fontSize: 20,
+            },
+            legend: {
+              display: true,
+              position: "right",
+            },
+          }}
+        />
+        <p>Single colorful Data Doughnut Graph</p>
+
+        <Doughnut
+          data={pieData}
+          options={{
+            title: {
+              display: true,
+              text: "Average Rainfall per month",
+              fontSize: 20,
+            },
+            legend: {
+              display: true,
+              position: "right",
+            },
+          }}
+        />
+        <p>Colorful Multi Data Doughnut Graph</p>
       </div>
     </div>
   );
