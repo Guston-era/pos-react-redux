c6869698f9a1b251a72a84e9e3a7a2cfde8bdb6d
GustoneraCodeBase <84548265+GustoneraCodeBase@users.noreply.github.com>
2021-12-11 03:18:47 +0300

    successfully added minimnal version of react js

--- src/screens/subscreens/StatusReport.js
+++ src/screens/subscreens/StatusReport.js
@@ -1,68 +1,32 @@
 import React from "react";
 import "../../css/subscreens.css";
 import MiniSideBarReports from "../../components/layout/MiniSideBarReports";
+import { Line } from "react-chartjs-2";
+import Chart from "chart.js/auto";
 
 const StatusReport = () => {
   return (
     <div className="flexed-content">
       <MiniSideBarReports active="status" />
       <div className="reports-panel">
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
-        <p> I am the status reports</p>
+        <Line
+          datasetIdKey="id"
+          data={{
+            labels: ["Jun", "Jul", "Aug"],
+            datasets: [
+              {
+                id: 1,
+                label: "m",
+                data: [5, 6, 7],
+              },
+              {
+                id: 2,
+                label: "d",
+                data: [3, 2, 1],
+              },
+            ],
+          }}
+        />
       </div>
     </div>
   );
